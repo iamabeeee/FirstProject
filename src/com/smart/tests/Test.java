2de41eaf2bf5d2002f897401dc656f6553ecd95d
iamabeeee <151390451+iamabeeee@users.noreply.github.com>
2024-01-06 14:29:46 -0500

    Update Test.java

--- src/com/smart/tests/Test.java
+++ src/com/smart/tests/Test.java
@@ -6,7 +6,6 @@ public class Test {
 
 	public void loginTest() {
 		//Adding a comment
-		//Ibrahim Abdulwahed
 		System.out.println("We are learing Git and GitHub through Eclipse.");
 		System.out.println("We had our initial commit.");
 	}
