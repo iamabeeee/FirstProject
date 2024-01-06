6a2dea40bcece28ddd583f765bda2272f2b1dcd6
iamabeeee <ibrahim.abdulwahed123@gmail.com>
2024-01-06 12:24:37 -0500

    I printed a new Statement.

--- src/com/smart/tests/Test.java
+++ src/com/smart/tests/Test.java
@@ -2,11 +2,10 @@ package com.smart.tests;
 
 public class Test {
 
-	
 	@org.testng.annotations.Test
-	
-	public void loginTest(){
-		
+
+	public void loginTest() {
+
 		System.out.println("We are learing Git and GitHub through Eclipse.");
 		System.out.println("We had our initial commit.");
 	}
