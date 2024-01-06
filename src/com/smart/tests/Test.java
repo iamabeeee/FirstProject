df9e0839628883bb95b625b5b23b5d4f8957b8cf
iamabeeee <151390451+iamabeeee@users.noreply.github.com>
2024-01-06 13:11:47 -0500

    Update Test.java

--- src/com/smart/tests/Test.java
+++ src/com/smart/tests/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	@org.testng.annotations.Test
 
 	public void loginTest() {
-
+		//Adding a comment
 		System.out.println("We are learing Git and GitHub through Eclipse.");
 		System.out.println("We had our initial commit.");
 	}
