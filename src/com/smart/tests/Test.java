245543af37e1f7c1160372c00b9f5d82fa67fac0
iamabeeee <ibrahim.abdulwahed123@gmail.com>
2024-01-06 12:15:54 -0500

    Initial Commit.

--- src/com/smart/tests/Test.java
+++ src/com/smart/tests/Test.java
@@ -2,4 +2,12 @@ package com.smart.tests;
 
 public class Test {
 
+	
+	@org.testng.annotations.Test
+	
+	public void loginTest(){
+		
+		System.out.println("We are learing Git and GitHub through Eclipse.");
+	}
+
 }
